1f2cec324247bf359ec6127f36c0f19030ab4cd9
tamana007 <tamana.efatwira1@gmail.com>
2024-02-13 23:44:44 -0800

    checkboxes

--- src/app/view-form/[formId]/page.jsx
+++ src/app/view-form/[formId]/page.jsx
@@ -1,29 +1,29 @@
+"use client";
 
-'use client'
-
-import React, { useEffect, useState, useRef } from 'react';
-import ReactDOMServer from 'react-dom/server';
-import html2pdf from 'html2pdf.js';
-import Logo from '@/components/Logo';
-import Print from '@/components/print'
+import React, { useEffect, useState, useRef } from "react";
+import ReactDOMServer from "react-dom/server";
+import html2pdf from "html2pdf.js";
+import Logo from "@/components/Logo";
+import Print from "@/components/print";
 function Page({ params }) {
   const id = params.formId;
   const [viewUser, setViewUser] = useState([]);
+  const [check,isChecked]=useState(true)
 
   useEffect(() => {
     const fetchFormData = async () => {
       try {
-        // Let's send params.formId to API 
+        // Let's send params.formId to API
         const res = await fetch(`/api/view-form?id=${params.formId}`);
 
         if (!res.ok) {
-          throw new Error('Failed to fetch data');
+          throw new Error("Failed to fetch data");
         }
         const data = await res.json();
         setViewUser(data.user);
-        console.log('check data', data);
+        console.log("check data", viewUser);
       } catch (error) {
-        console.error('Error fetching form data:', error);
+        console.error("Error fetching form data:", error);
       }
     };
 
@@ -36,41 +36,124 @@ function Page({ params }) {
 
     return (
       <div className="print-container">
-      <Logo />
-      <div className="print-form-container">
-        <h1>View Form</h1>
-        <div className="checkboxes">
-          <label htmlFor="data1">
-            <input type="checkbox" id="data1" name="data1" />
-            Data One
-          </label>
-          <div>
-          <label htmlFor="data2">
-          <input type="checkbox" id="data2" name="data2" />
-            Data Two
-          </label>
+        <Logo />
+        <div className="print-form-container">
+          <h1>View Form</h1>
+          <div className="checkboxes">
+            <label htmlFor="data1">
+              <input type="checkbox" id="data1" name="data1" checked={true} />
+              Social Media Ad
+            </label>
+            <div>
+              <label htmlFor="data2">
+                <input type="checkbox" id="data2" name="data2" />
+                Bussiness Card
+              </label>
+            </div>
+            <div>
+              <label htmlFor="data2">
+                <input type="checkbox" id="data2" name="data2"  />
+                Flyer 
+              </label>
+            </div>
+            <div>
+              <label htmlFor="data2">
+                <input type="checkbox" id="data2" name="data2" />
+                Brochure
+              </label>
+            </div>
+            <div>
+              <label htmlFor="data2">
+                <input type="checkbox" id="data2" name="data2" />
+                Pullup Banner
+              </label>
+            </div>
+            <div>
+              <label htmlFor="data2">
+                <input type="checkbox" id="data2" name="data2" />
+                Table top Banner
+              </label>
+            </div>
+            <div>
+              <label htmlFor="data2">
+                <input type="checkbox" id="data2" name="data2" />
+                Special Merchandise/Swag item
+              </label>
+            </div>
+            <div>
+              <label htmlFor="data2">
+                <input type="checkbox" id="data2" name="data2" />
+                Placard
+              </label>
+            </div>
+            <div>
+              <label htmlFor="data2">
+                <input type="checkbox" id="data2" name="data2" />
+                Marketing
+              </label>
+            </div>
+            <div>
+              <label htmlFor="data2">
+                <input type="checkbox" id="data2" name="data2" />
+                Any other (Please Specify)
+              </label>
+            </div>
+            <div>
+              <label htmlFor="data2">
+                <input type="checkbox" id="data2" name="data2" />
+                What is your program name?
+              </label>
+            </div>
 
+            {/* Add more checkboxes as needed */}
           </div>
-           
-          {/* Add more checkboxes as needed */}
+          <div className="questions">
+            <p>
+              Question 1: What is your program name?
+              <br />
+              Answer: {viewUser.programName} <p type="text" name="answer1" />
+            </p>
+            <p>
+              Question 2: Your Name:
+              <br />
+              Answer: {viewUser.name}<p type="text" name="answer2" />
+            </p>
+            {/* Add more questions and answers as needed */}
+            <p>
+              Question 2: Director Email Address:
+              <br />
+              Answer: {viewUser.directorEmail}<p type="text" name="answer2" />
+            </p>
+            <p>
+              Question 2: Please Specify the Size and Quantity (If relevant)
+              <br />
+              Answer: {viewUser.size}<p type="text" name="answer2" />
+            </p>
+            <p>
+              Question 2: Please Write any side-note for Social Media Post,Flyer/Brochure/Swag Item (If relevant)
+              <br />
+              Answer: {viewUser.sideNote}<p type="text" name="answer2" />
+            </p>
+            <p>
+              Question 2: Amount Approved?
+              <br />
+              Answer: {viewUser.approvedAmount}<p type="text" name="answer2" />
+            </p>
+            <p>
+              Question 2: Budget approval by the Accounts Department?
+              <br />
+              Answer: {viewUser.budgetApprovalByAccount}<p type="text" name="answer2" />
+            </p>
+            <p>
+              Question 2: Invoince to be made under which Name/program ?
+              <br />
+              Answer: {viewUser.invoiceTobeMade}<p type="text" name="answer2" />
+            </p>
+          </div>
+         
         </div>
-        <div className="questions">
-          <p>
-            Question 1: What is your name?
-            <br />
-            Answer: <input type="text" name="answer1" />
-          </p>
-          <p>
-            Question 2: Where are you from?
-            <br />
-            Answer: <input type="text" name="answer2" />
-          </p>
-          {/* Add more questions and answers as needed */}
-        </div>
-        <button type="submit">Submit</button>
       </div>
-    </div>
-      
+
       // <div className="print-container">
       //   <Logo />
       //   <p>Helloooooo {viewUser.programName}</p>
@@ -86,10 +169,10 @@ function Page({ params }) {
     return new Promise((resolve, reject) => {
       const options = {
         margin: 10,
-        filename: 'exported_document.pdf',
-        image: { type: 'jpeg', quality: 0.98 },
+        filename: "exported_document.pdf",
+        image: { type: "jpeg", quality: 0.98 },
         html2canvas: { scale: 2 },
-        jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+        jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
       };
       html2pdf()
         .from(printElement)
@@ -106,42 +189,126 @@ function Page({ params }) {
         <p>Helloooooo {viewUser.programName}</p>
         <h1>This is my View Form page and form Id is: {params.formId}</h1>
       </div> */}
-        <div className="print-container">
-      <Logo />
-      <div className="print-form-container">
-        <h1>View Form</h1>
-        <div className="checkboxes">
-          <label htmlFor="data1">
-            <input type="checkbox" id="data1" name="data1" />
-            Data One
-          </label>
-          <div>
-          <label htmlFor="data2">
-          <input type="checkbox" id="data2" name="data2" />
-            Data Two
-          </label>
+    <div className="print-container">
+        <Logo />
+        <div className="print-form-container">
+          <h1>View Form</h1>
+          <div className="checkboxes">
+            <label htmlFor="data1">
+              <input type="checkbox" id="data1" name="data1" />
+              Social Media Ad
+            </label>
+            <div>
+              <label htmlFor="data2">
+                <input type="checkbox" id="data2" name="data2"  />
+                Bussiness Card
+              </label>
+            </div>
+            <div>
+              <label htmlFor="data2">
+                <input type="checkbox" id="data2" name="data2" />
+                Flyer
+              </label>
+            </div>
+            <div>
+              <label htmlFor="data2">
+                <input type="checkbox" id="data2" name="data2" />
+                Brochure
+              </label>
+            </div>
+            <div>
+              <label htmlFor="data2">
+                <input type="checkbox" id="data2" name="data2" />
+                Pullup Banner
+              </label>
+            </div>
+            <div>
+              <label htmlFor="data2">
+                <input type="checkbox" id="data2" name="data2" />
+                Table top Banner
+              </label>
+            </div>
+            <div>
+              <label htmlFor="data2">
+                <input type="checkbox" id="data2" name="data2" />
+                Special Merchandise/Swag item
+              </label>
+            </div>
+            <div>
+              <label htmlFor="data2">
+                <input type="checkbox" id="data2" name="data2" />
+                Placard
+              </label>
+            </div>
+            <div>
+              <label htmlFor="data2">
+                <input type="checkbox" id="data2" name="data2" />
+                Marketing
+              </label>
+            </div>
+            <div>
+              <label htmlFor="data2">
+                <input type="checkbox" id="data2" name="data2" />
+                Any other (Please Specify)
+              </label>
+            </div>
+            <div>
+              <label htmlFor="data2">
+                <input type="checkbox" id="data2" name="data2" />
+                What is your program name?
+              </label>
+            </div>
 
+            {/* Add more checkboxes as needed */}
           </div>
-           
-          {/* Add more checkboxes as needed */}
-        </div>
-        <div className="questions">
-          <p>
-            Question 1: What is your name?
-            <br />
-            Answer: <input type="text" name="answer1" />
-          </p>
-          <p>
-            Question 2: Where are you from?
-            <br />
-            Answer: <input type="text" name="answer2" />
-          </p>
-          {/* Add more questions and answers as needed */}
-        </div>
-        <button type="submit">Submit</button>
-      </div>
-    </div>
+          <div className="questions">
+            <p>
+              Question 1: What is your program name?
+              <br />
+              Answer: {viewUser.programName} <p type="text" name="answer1" />
+            </p>
+            <p>
+              Question 2: Your Name:
+              <br />
+              Answer: {viewUser.name}<p type="text" name="answer2" />
+            </p>
+            {/* Add more questions and answers as needed */}
+            <p>
+              Question 2: Director Email Address:
+              <br />
+              Answer: {viewUser.directorEmail}<p type="text" name="answer2" />
+            </p>
+            <p>
+              Question 2: Please Specify the Size and Quantity (If relevant)
+              <br />
+              Answer: {viewUser.size}<p type="text" name="answer2" />
+            </p>
+            <p>
+              Question 2: Please Write any side-note for Social Media Post,Flyer/Brochure/Swag Item (If relevant)
+              <br />
+              Answer: {viewUser.sideNote}<p type="text" name="answer2" />
+            </p>
+            <p>
+              Question 2: Amount Approved?
+              <br />
+              Answer: {viewUser.approvedAmount}<p type="text" name="answer2" />
+            </p>
+            <p>
+              Question 2: Budget approval by the Accounts Department?
+              <br />
+              Answer: {viewUser.budgetApprovalByAccount}<p type="text" name="answer2" />
+            </p>
+            <p>
+              Question 2: Invoince to be made under which Name/program ?
+              <br />
+              Answer: {viewUser.invoiceTobeMade}<p type="text" name="answer2" />
+            </p>
+          </div>
+          {/* <button type="submit">Submit</button> */}
       <button onClick={() => exportAsPDF()}>Export as PDF</button>
+
+        </div>
+      </div>
     </div>
   );
 }
--- src/data/options.js
+++ src/data/options.js
@@ -52,7 +52,7 @@ const questions = [
   },
   {
     description: 'Your Name:',
-    key: 'name'
+    key: 'Your Name:'
   },
   {
     description: 'Director Email Address:',
