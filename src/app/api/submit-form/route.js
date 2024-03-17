80586a65b7073dfd610a155d264ebe471d412004
tamana007 <tamana.efatwira1@gmail.com>
2024-03-17 16:53:50 -0700

    add file type

--- src/app/api/submit-form/route.js
+++ src/app/api/submit-form/route.js
@@ -15,143 +15,147 @@ export async function POST(request) {
   const file1 = formData.get("file1");
   const file2 = formData.get("file2");
 
-  // console.log('File 2',file2);
-  const directorEmail = formData.get("directorEmail");
+  try {
+    // console.log('File 2',file2);
+    const directorEmail = formData.get("directorEmail");
 
-  formData.delete("file1");
-  formData.delete("file2");
-  formData.delete("directorEmail");
+    formData.delete("file1");
+    formData.delete("file2");
+    formData.delete("directorEmail");
 
-  let arrayBuffer1, arrayBuffer2, buffer1, buffer2, mimeType, mimeType2;
-  let file1base64String = null;
-  let file2base64String = null;
+    let arrayBuffer1, arrayBuffer2, buffer1, buffer2, mimeType, mimeType2;
+    let file1base64String = null;
+    let file2base64String = null;
 
-  if (!!file1) {
-    arrayBuffer1 = await file1.arrayBuffer();
-    buffer1 = Buffer.from(arrayBuffer1);
-    file1base64String = buffer1.toString("base64");
-    mimeType = mimeTypes.lookup(file1.name) || "application/octet-stream";
-  }
-  
-  if (!!file2) {
-    arrayBuffer2 = await file2.arrayBuffer();
-    buffer2 = Buffer.from(arrayBuffer2);
-    file2base64String = buffer2.toString("base64");
-    mimeType2 = mimeTypes.lookup(file2.name) || "application/octet-stream";
-  }
+    if (!!file1) {
+      arrayBuffer1 = await file1.arrayBuffer();
+      buffer1 = Buffer.from(arrayBuffer1);
+      file1base64String = buffer1.toString("base64");
+      mimeType = mimeTypes.lookup(file1.name) || "application/octet-stream";
+    }
 
-  // Change formData to an object...
-  const formDataObject = {};
+    if (!!file2) {
+      arrayBuffer2 = await file2.arrayBuffer();
+      buffer2 = Buffer.from(arrayBuffer2);
+      file2base64String = buffer2.toString("base64");
+      mimeType2 = mimeTypes.lookup(file2.name) || "application/octet-stream";
+    }
 
-  let directorsEmail = "";
-  for (const [key, value] of formData.entries()) {
-    // Check if the value is not a File object
-    if (!(value instanceof File)) {
-      if (key == "directorEmail") {
-        directorsEmail = value;
+    // Change formData to an object...
+    const formDataObject = {};
+
+    let directorsEmail = "";
+    for (const [key, value] of formData.entries()) {
+      // Check if the value is not a File object
+      if (!fs.existsSync(value)) {
+        if (key == "directorEmail") {
+          directorsEmail = value;
+        }
+        formDataObject[key] = value;
       }
-      formDataObject[key] = value;
     }
-  }
 
-  // Change string to boolean...
-  Object.keys(formDataObject).forEach((item) => {
-    if (formDataObject[item] === "true") {
-      formDataObject[item] = true;
-    } else if (formDataObject[item] === "false") {
-      formDataObject[item] = false;
-    }
-  });
-
-  const plainFormDataObject = reorderObject(formDataObject);
-
-  formDataObject.attachementMimeType = mimeType;
-  formDataObject.secondAttachementMimeType = mimeType2;
-
-  formDataObject.attachement = file1base64String;
-  formDataObject.secondAttachement = file2base64String;
-  formDataObject.directorEmail = directorEmail;
-
-  // DATABASE SETUP.....
-  // console.log("formData", formDataObject);
-  const res = await new User(formDataObject).save();
-  const id = res._id;
-
-  const url = `http://localhost:3000/director-review?id=${id}&email=${directorEmail}`;
-
-  // Email configuration....
-  const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: "tamana.efatwira1@gmail.com",
-      pass: "ubjziyxjvhqxmkaa",
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
-  });
-  let htmlContent = "<h1>Marketing Request Form</h1>";
-  htmlContent +=
-    "<h2>All requests need to be submitted at least one-week in advance</h2>"; // Added h2 tag
-  htmlContent += "<ul style='list-style-type: none; padding: 0;'>";
-  Object.entries(plainFormDataObject).forEach(([question, answer]) => {
-    if (typeof answer === "boolean") {
-      // If the answer is a boolean, add it to booleanQuestionsHtml
-      htmlContent += `<li style='margin-bottom: 10px;'><strong>${question}:</strong> <input type="checkbox" style='margin-left: 5px;' ${
-        answer ? "checked" : ""
-      } disabled></li>`;
-    }
-    if (
-      question !== "attachement" &&
-      question !== "secondAttachement" &&
-      typeof answer !== "boolean"
-    ) {
-      // If the answer is not a boolean, add it to otherQuestionsHtml
-      htmlContent += `<li style='margin-bottom: 10px;'><strong>${question}:</strong> <div style="display: block;">${answer}</div></li>`;
-    }
-  });
-  htmlContent += "</ul>"; // Close the unordered list
-  htmlContent += `<p> Please click on this link for reivewing application: ${url}`;
-
-  const mailOptions = {
-    from: "tamana.efatwira1@gmail.com",
-    to: directorEmail,
-    subject: "Marketing E-request Form...",
-    text: "Please find the attached file.",
-    html: htmlContent,
-    attachments: [],
-  };
-
-  // Add file1 attachment if it exists
-  if (file1) {
-    mailOptions.attachments.push({
-      filename: file1.name,
-      content: buffer1, // buffer1 is the buffer for the first attachment
-      encoding: "base64",
-      contentType: mimeType,
+    // Change string to boolean...
+    Object.keys(formDataObject).forEach((item) => {
+      if (formDataObject[item] === "true") {
+        formDataObject[item] = true;
+      } else if (formDataObject[item] === "false") {
+        formDataObject[item] = false;
+      }
     });
-  }
 
-  // Add file2 attachment if it exists
-  if (file2) {
-    mailOptions.attachments.push({
-      filename: file2.name,
-      content: buffer2, // buffer2 is the buffer for the second attachment
-      encoding: "base64",
-      contentType: mimeType2,
+    const plainFormDataObject = reorderObject(formDataObject);
+
+    formDataObject.attachementMimeType = mimeType;
+    formDataObject.secondAttachementMimeType = mimeType2;
+
+    formDataObject.attachement = file1base64String;
+    formDataObject.secondAttachement = file2base64String;
+    formDataObject.directorEmail = directorEmail;
+
+    // DATABASE SETUP.....
+    // console.log("formData", formDataObject);
+    const res = await new User(formDataObject).save();
+    const id = res._id;
+
+    const url = `http://localhost:3000/director-review?id=${id}&email=${directorEmail}`;
+
+    // Email configuration....
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "tamana.efatwira1@gmail.com",
+        pass: "ubjziyxjvhqxmkaa",
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
     });
-  }
+    let htmlContent = "<h1>Marketing Request Form</h1>";
+    htmlContent +=
+      "<h2>All requests need to be submitted at least one-week in advance</h2>"; // Added h2 tag
+    htmlContent += "<ul style='list-style-type: none; padding: 0;'>";
+    Object.entries(plainFormDataObject).forEach(([question, answer]) => {
+      if (typeof answer === "boolean") {
+        // If the answer is a boolean, add it to booleanQuestionsHtml
+        htmlContent += `<li style='margin-bottom: 10px;'><strong>${question}:</strong> <input type="checkbox" style='margin-left: 5px;' ${
+          answer ? "checked" : ""
+        } disabled></li>`;
+      }
+      if (
+        question !== "attachement" &&
+        question !== "secondAttachement" &&
+        typeof answer !== "boolean"
+      ) {
+        // If the answer is not a boolean, add it to otherQuestionsHtml
+        htmlContent += `<li style='margin-bottom: 10px;'><strong>${question}:</strong> <div style="display: block;">${answer}</div></li>`;
+      }
+    });
+    htmlContent += "</ul>"; // Close the unordered list
+    htmlContent += `<p> Please click on this link for reivewing application: ${url}`;
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error("Error sending email: ", error);
-    } else {
-      console.log("Email sent: ", info.response);
+    const mailOptions = {
+      from: "tamana.efatwira1@gmail.com",
+      to: directorEmail,
+      subject: "Marketing E-request Form...",
+      text: "Please find the attached file.",
+      html: htmlContent,
+      attachments: [],
+    };
+
+    // Add file1 attachment if it exists
+    if (file1) {
+      mailOptions.attachments.push({
+        filename: file1.name,
+        content: buffer1, // buffer1 is the buffer for the first attachment
+        encoding: "base64",
+        contentType: mimeType,
+      });
     }
-  });
+
+    // Add file2 attachment if it exists
+    if (file2) {
+      mailOptions.attachments.push({
+        filename: file2.name,
+        content: buffer2, // buffer2 is the buffer for the second attachment
+        encoding: "base64",
+        contentType: mimeType2,
+      });
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Error sending email: ", error);
+      } else {
+        console.log("Email sent: ", info.response);
+      }
+    });
+  } catch (e) {
+    console.log(e);
+  }
 
   return Response.json("Email Has been Sent...");
 }
--- src/components/SuccessMessage.jsx
+++ src/components/SuccessMessage.jsx
@@ -8,45 +8,49 @@ import styles from '@/Style/successsMessage.module.css'
 function SuccessMessage() {
   const [showMessage, setShowMessage] = useState(true);
   const [closed, setClosed] = useState(false);
-  const { isSuccess, setIsSuccess } = useSuccsess();
+  const { isSuccess, setIsSuccess } = useState(true);
 
   const handleClose = () => {
     setShowMessage(false);
     setClosed(true);
-    setIsSuccess(false);
+    // setIsSuccess(false);
   };
 
   return (
-    <div className={styles.successContainer}>
-      {/* <div className= {styles.modalPopup}>
-        <Paper
-          elevation={5}
-          style={{
-            position: "fixed",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            width: "300px",
-            padding: "16px",
-            textAlign: "center",
-            backgroundColor: "#f0f8ff",
-            borderRadius: "8px",
-            boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
-            zIndex: 9999,
-          }}
-        >
-          <Typography
-            variant="h6"
-            style={{ color: "green", marginBottom: "8px" }}
-          >
-            Your message has been successfully sent!
-          </Typography>
-          <Button variant="contained" color="primary" onClick={handleClose}>
-            Close
-          </Button>
-        </Paper>
-      </div> */}
-    </div>
+    <>
+      {showMessage && (
+        <div className={styles.successContainer}>
+          <div>
+            <Paper
+              elevation={5}
+              style={{
+                position: "fixed",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                width: "300px",
+                padding: "16px",
+                textAlign: "center",
+                backgroundColor: "#f0f8ff",
+                borderRadius: "8px",
+                boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+                zIndex: 9999,
+              }}
+            >
+              <Typography
+                variant="h6"
+                style={{ color: "green", marginBottom: "8px" }}
+              >
+                Your message has been successfully sent!
+              </Typography>
+              <Button variant="contained" color="primary" onClick={handleClose}>
+                Close
+              </Button>
+            </Paper>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
